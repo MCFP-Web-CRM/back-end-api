c52214b0f65784104413a9ff143ebc9895c64d40
Frank Park <jehyn923@gmail.com>
2021-10-12 18:20:30 +0900

    chore: 프로젝트 생성

---
+++ src/test/java/com/mcfuturepartners/crm/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcfuturepartners.crm.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
